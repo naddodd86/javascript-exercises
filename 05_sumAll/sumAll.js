a391661db7f5fd82b122c8ca304f21b814e081d9
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-29 21:16:49 -0600

    add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
 
+const sumAll = function(a, b) {
+	
+	let sum = 0;
+	if (a <= 0 || b <= 0)	{
+		return 'ERROR'
+		
+	} else if (Number(a) !== a || Number(b) !== b)	{
+		return 'ERROR'
+	
+	} else if ( a > b)	{
+		for (i = a; i >= b; i--)	{
+			sum += i;
+		}
+	} else {
+		for (i = a; i <= b; i++)	{
+			sum += i;
+		}
+	}
+	return sum;
 };
+			
+
 
 // Do not edit below this line
 module.exports = sumAll;
