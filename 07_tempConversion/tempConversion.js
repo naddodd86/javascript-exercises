97ff50d0be027f9eaff6fad079233395f2c649c6
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-29 21:23:25 -0600

    add tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const fTemp = fahrenheit;
+  const cTemp = (5/9) * (fTemp - 32);
+  return Math.round(cTemp * 10) / 10;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const cTemp = celsius;
+  const fTemp = (cTemp * 1.8) + 32;
+  return Math.round(fTemp * 10) /10;
 };
 
 // Do not edit below this line
