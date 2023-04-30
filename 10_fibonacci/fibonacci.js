f19cef99ac249a4285f721c755155a97de0da0ee
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-29 21:30:19 -0600

    add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if (num < 0) return 'OOPS';
+	let a= 1;
+	let b = 1;
+	for (let i = 3; i <= num; i++)	{
+		let c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
 };
 
 // Do not edit below this line
