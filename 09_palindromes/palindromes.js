0fbe9aa982c4df0b9616da939c47f43092d9e9b0
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-29 21:28:51 -0600

    add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const re = /[\W_]/g;
+	const lowRegStr = str.toLowerCase().replace(re, '');
+	const reverseStr = lowRegStr.split('').reverse().join('');
+	return reverseStr === lowRegStr;
+	
 };
 
 // Do not edit below this line
